148948ae35a3a6cc6dae192cfcd1635a896e4baa
Ken Lenhart <omnipaid@gmail.com>
2022-07-12 14:57:29 -0700

    cleanup: cell_spec.rb add class instances to before do block

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Cell do
   before :each do 
     @cell = Cell.new("C4")
     @cell_1 = Cell.new("B4")
+    @cell_2 = Cell.new("C3")
     @cruiser = Ship.new("Cruiser", 3)
   end
   it 'exists' do 
@@ -48,16 +49,15 @@ RSpec.describe Cell do
     expect(@cell_1.render).to eq(".")
     @cell_1.fire_upon
     expect(@cell_1.render).to eq("M")
-    cell_2 = Cell.new("C3")
-    cell_2.place_ship(@cruiser)
-    expect(cell_2.render).to eq(".")
-    expect(cell_2.render(true)).to eq("S")
-    cell_2.fire_upon
-    expect(cell_2.render).to eq("H")
+    @cell_2.place_ship(@cruiser)
+    expect(@cell_2.render).to eq(".")
+    expect(@cell_2.render(true)).to eq("S")
+    @cell_2.fire_upon
+    expect(@cell_2.render).to eq("H")
     expect(@cruiser.sunk?).to eq(false)
     @cruiser.hit 
     @cruiser.hit
     expect(@cruiser.sunk?).to eq(true)
-    expect(cell_2.render).to eq("X")
+    expect(@cell_2.render).to eq("X")
   end
 end
