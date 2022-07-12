8381d6bb13912f180fe9be85d863020a4320ed5e
Carter Ball <carter.k.ball@gmail.com>
2022-07-12 17:31:00 -0400

    Create method render with optional input and test to ensure render works as expected

--- lib/cell.rb
+++ lib/cell.rb
@@ -12,7 +12,6 @@ class Cell
   end
   def place_ship(ship_object)
     @ship = ship_object
-    # unexpected return of ship class value
   end
   def fired_upon?
     fired_upon_logic = true
@@ -24,20 +23,18 @@ class Cell
       self.ship.hit
     end
   end
-  def render
+  def render(variable = nil)
     if self.empty? == true && self.fired_upon? == true
       p "M"
-        # if self.empty? == false
-          #   if self.ship.sunk? == true
-          #     p "X"
-          #     # elsif self.fired_upon? == true
-          #     #   p "H"
-          #   end 
-    
-    
- 
-    else 
+    elsif variable == true
+      p "S"
+    elsif self.empty? == false && self.ship.sunk? == true
+      p "X"
+    elsif self.empty? == false && self.fired_upon? == true
+      p "H"
+    else
       p "."
     end
   end
 end
+
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -48,5 +48,16 @@ RSpec.describe Cell do
     expect(@cell_1.render).to eq(".")
     @cell_1.fire_upon
     expect(@cell_1.render).to eq("M")
+    cell_2 = Cell.new("C3")
+    cell_2.place_ship(@cruiser)
+    expect(cell_2.render).to eq(".")
+    expect(cell_2.render(true)).to eq("S")
+    cell_2.fire_upon
+    expect(cell_2.render).to eq("H")
+    expect(@cruiser.sunk?).to eq(false)
+    @cruiser.hit 
+    @cruiser.hit
+    expect(@cruiser.sunk?).to eq(true)
+    expect(cell_2.render).to eq("X")
   end
 end
