3d405a21f73b23e6a7a027ef4c929472890dbd50
Ken Lenhart <omnipaid@gmail.com>
2022-07-12 11:26:36 -0700

    Add cell render method

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,14 +3,15 @@ require './lib/cell'
 
 RSpec.describe Cell do
   before :each do 
-    @cell = Cell.new("B4")
+    @cell = Cell.new("C4")
+    @cell_1 = Cell.new("B4")
     @cruiser = Ship.new("Cruiser", 3)
   end
   it 'exists' do 
     expect(@cell).to be_a(Cell)
   end
   it 'has coordinates' do 
-    expect(@cell.coordinate).to eq("B4")
+    expect(@cell.coordinate).to eq("C4")
   end
   it 'had ship' do
     expect(@cell.ship).to eq(nil)
@@ -39,8 +40,13 @@ RSpec.describe Cell do
     expect(@cell.ship.sunk?).to eq(true)
   end
   it 'fire upon works without ship present' do
-    cell = Cell.new("B4")
-    cell.fire_upon
-    expect(cell.fired_upon?).to eq(true)
+    @cell = Cell.new("B4")
+    @cell.fire_upon
+    expect(@cell.fired_upon?).to eq(true)
+  end
+  it 'does cell render method' do
+    expect(@cell_1.render).to eq(".")
+    @cell_1.fire_upon
+    expect(@cell_1.render).to eq("M")
   end
 end
